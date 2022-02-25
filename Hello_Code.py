e5e4ea03d65b96b0a5a498685193ab1fcb0813a3
adriana soares <adrianasoares@Air-di-adriana.lan>
2022-02-25 14:37:21 +0100

    Hello Code

---
+++ Hello_Code.py
@@ -0,0 +1,5 @@
+print('Ciao Mundo!')
+#Questa è la mia prima lezione di Python
+print(1+1)
+#Una lettera per la Adriana di qua a 6 mesi, scritta traversso il Python
+print(' Ciao bella, come ti senti? In verità questa é una domanda stupida, lo so che sentiti bene. Hai raggiunto un tuo obbiettivo di lavoro. Sei bravissima! Per adesso va bene il tuo luogo, ma sai bene che deve svillupare ed avere una certificazione di proget management perche quello sará il tuo prossimo inizio... Stai arrivando piano pianop nel posto che sempre hai sognato, e meglio, in Italia. Hai fatto a tanti di chiudere la bocca. Però, sappiamo bene che quello è il meno importante. La cosa più bella è dare tanta gioia al tuo foglio e la tua mamma che sono così fieri di te! Io no mi sono mai permessa di dubbitare di te, ti conosco bene; sopratutto la tua potenza e capacità. Dai ragazza, spacca tutto!!!!! ')
